5168d8ebb1572fead977ea55a5b4d4c7644a8584
ayudaslani <ayushidaslani75@gmail.com>
2026-02-12 23:40:58 +0530

    12-2-26

---
+++ PR-9-role-api/src/config/db.connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const dbconnect = () => {
+    mongoose.connect('mongodb+srv://ayushidaslani:ayushidaslani29@cluster0.d66cwov.mongodb.net/roleapi')
+        .then(() => console.log('DB is Connected !!!'))
+        .catch((err) => console.log(err))
+}
+
+module.exports = dbconnect;
